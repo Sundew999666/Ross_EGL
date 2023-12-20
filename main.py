10e263e3b769b3e8c0ae82ec23cea48e829ad0ec
2026gunaev.ra <sunlive999666@gmail.com>
2023-12-20 15:08:17 +0300

    Commit_Test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-#создаем детектор
+
 handsDetector = mp.solutions.hands.Hands()
 cap = cv2.VideoCapture(0)
 while(cap.isOpened()):
@@ -10,23 +10,49 @@ while(cap.isOpened()):
     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
         break
     flipped = np.fliplr(frame)
-    # переводим его в формат RGB для распознавания
     flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
-    # Распознаем
     results = handsDetector.process(flippedRGB)
-    # Рисуем распознанное, если распозналось
     if results.multi_hand_landmarks is not None:
-        # нас интересует только подушечка указательного пальца (индекс 8)
-        # нужно умножить координаты а размеры картинки
-        x_tip = int(results.multi_hand_landmarks[0].landmark[8].x *
+        x_tip = int(results.multi_hand_landmarks[0].landmark[4].x *
+                     flippedRGB.shape[1])
+        y_tip = int(results.multi_hand_landmarks[0].landmark[4].y *
+                     flippedRGB.shape[0])
+        x1_tip = int(results.multi_hand_landmarks[0].landmark[8].x *
                 flippedRGB.shape[1])
-        y_tip = int(results.multi_hand_landmarks[0].landmark[8].y *
+        y1_tip = int(results.multi_hand_landmarks[0].landmark[8].y *
                 flippedRGB.shape[0])
+        x2_tip = int(results.multi_hand_landmarks[0].landmark[12].x *
+                    flippedRGB.shape[1])
+        y2_tip = int(results.multi_hand_landmarks[0].landmark[12].y *
+                    flippedRGB.shape[0])
+        x3_tip = int(results.multi_hand_landmarks[0].landmark[16].x *
+                    flippedRGB.shape[1])
+        y3_tip = int(results.multi_hand_landmarks[0].landmark[16].y *
+                    flippedRGB.shape[0])
+        x4_tip = int(results.multi_hand_landmarks[0].landmark[20].x *
+                     flippedRGB.shape[1])
+        y4_tip = int(results.multi_hand_landmarks[0].landmark[20].y *
+                     flippedRGB.shape[0])
         cv2.circle(flippedRGB,(x_tip, y_tip), 10, (255, 0, 0), -1)
-        print(results.multi_hand_landmarks[0])
-    # переводим в BGR и показываем результат
+        cv2.circle(flippedRGB, (x1_tip, y1_tip), 10, (0, 255, 0), -1)
+        cv2.circle(flippedRGB, (x2_tip, y2_tip), 10, (0, 0, 255), -1)
+        cv2.circle(flippedRGB, (x3_tip, y3_tip), 10, (0, 0, 0), -1)
+        cv2.circle(flippedRGB, (x4_tip, y4_tip), 10, (255, 255, 255), -1)
+        print(f'БОЛЬШОЙ {x_tip}, {y_tip}', f'УКАЗАТЕЛЬНЫЙ {x1_tip}, {y1_tip};', f'СРЕДНИЙ {x2_tip}, {y2_tip};', f'БЕЗЫМЯННЫЙ {x3_tip}, {y3_tip};', f'МИЗИНЕЦ {x4_tip}, {y4_tip}.')
+        if (abs(x1_tip-x2_tip) < 30 and y1_tip > 250 and y2_tip > 250):
+            print('N')
+            cv2.putText(flippedRGB, 'LETTER: N', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
+        elif (abs(x1_tip-x2_tip) < 30 and y1_tip < 250 and y2_tip < 250):
+            print('U')
+            cv2.putText(flippedRGB, 'LETTER: U', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
+        elif (abs(y_tip-y1_tip) < 70 and abs(y_tip-y2_tip) < 80 and abs(x1_tip-x2_tip) > 50 and abs(x_tip-x1_tip) < 40):
+            print('K')
+            cv2.putText(flippedRGB, 'LETTER: K', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
+        elif (abs(x1_tip-x2_tip) > 50):
+            print('V')
+            cv2.putText(flippedRGB, 'LETTER: V', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0))
+
     res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
     cv2.imshow("Hands", res_image)
 
-# освобождаем ресурсы
 handsDetector.close()
